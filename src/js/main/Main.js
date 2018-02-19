7458e53726da6c712846d73183d425deed887c98
akado117 <akaidan@mtu.edu>
2018-02-19 13:17:03 -0700

    Added in test and initial implementation for parseFileInput

--- src/js/main/Main.js
+++ src/js/main/Main.js
@@ -2,4 +2,11 @@ export default class Main {
     constructor() {
         this.trips = new Map();
     }
+    parseFileInput(inputString) {
+        const splitCommands = inputString.split(/\r?\n/);
+
+        this.parsedInput = splitCommands;
+
+        return splitCommands;
+    }
 }
--- src/js/main/Main.test.js
+++ src/js/main/Main.test.js
@@ -10,4 +10,12 @@ describe('Main Class', () => {
             expect(main.trips).toEqual(new Map());
         });
     });
+    describe('parseFileInput', () => {
+        const sampleInput = 'Driver Dan\nDriver Alex\nDriver Bob\nTrip Dan 07:15 07:45 17.3\nTrip Dan 06:12 06:32 21.8\n' +
+            'Trip Alex 12:01 13:16 42.0';
+        it('should parse input so each new line is a new string in an array and set it to internal state', () => {
+            expect(main.parseFileInput(sampleInput).length).toBe(6);
+            expect(main.parsedInput.length).toBe(6);
+        });
+    });
 });
